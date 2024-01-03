9e2570f2d68ac6f80b7e79460891581391ffef47
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-01-03 10:52:16 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 76 ms, Memory: 6132 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cs
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cs
@@ -8,43 +8,43 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            int length = int.Parse(Console.ReadLine());
+                int length = int.Parse(Console.ReadLine());
             int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 
             int maxCount = 1;
             // 특정 포인트를 기준으로 최고의 길이를 찾아보자
-            for (int k = 0; k < length; k++)
+            // 가장 긴 부분수열 문제를 풀어보자
+            int[] dpUp = new int[length];
+            Array.Fill(dpUp, 1);
+
+            for (int i = 1; i < length; i++)
             {
-                // 가장 긴 부분수열 문제를 풀어보자
-                int[] dpUp = new int[k + 1];
-                Array.Fill(dpUp, 1);
-
-                for (int i = 1; i < k + 1; i++)
+                for (int j = 0; j < i; j++)
                 {
-                    for (int j = 0; j < i; j++)
-                    {
-                        if (nums[i] > nums[j])
-                            dpUp[i] = Math.Max(dpUp[i], dpUp[j] + 1);
-                    }
+                    if (nums[i] > nums[j])
+                        dpUp[i] = Math.Max(dpUp[i], dpUp[j] + 1);
                 }
-
-                // 반대 방향에서 확인하여 보자
-                int[] dpDown = new int[length - k];
-                Array.Fill(dpDown, 1);
-
-                for (int i = length - k - 2; i >= 0; i--)
-                {
-                    for (int j = length - k - 1; j > i; j--)
-                    {
-                        if (nums[i + k] > nums[j + k])
-                            dpDown[i] = Math.Max(dpDown[i], dpDown[j] + 1);
-                    }
-                }
-
-                // Console.WriteLine($"{(dpUp.Length > 0 ? nums[k] : 0)} : {(dpUp.Length > 0 ? dpUp[k] : 0)} ||| {(dpDown.Length > 0 ? dpDown[0] : 0)}");
-                maxCount = Math.Max(maxCount, (dpUp.Length > 0 ? dpUp[k] : 0) + (dpDown.Length > 0 ? dpDown[0] : 0) );
             }
-            Console.WriteLine(maxCount - 1);
+
+            // 반대 방향에서 확인하여 보자
+            int[] dpDown = new int[length];
+            Array.Fill(dpDown, 1);
+
+            for (int i = length - 2; i >= 0; i--)
+            {
+                for (int j = length - 1; j > i; j--)
+                {
+                    if (nums[i] > nums[j])
+                        dpDown[i] = Math.Max(dpDown[i], dpDown[j] + 1);
+                }
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                maxCount = Math.Max(maxCount, dpUp[i] + dpDown[i] - 1);
+            }
+        
+        Console.WriteLine(maxCount);
         }
     }
 }
