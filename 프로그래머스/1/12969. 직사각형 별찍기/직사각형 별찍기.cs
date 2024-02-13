5a958425bb063ad976765ce8f884fca72198817f
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-02-13 10:22:54 +0900

    [level 1] Title: 직사각형 별찍기, Time: 502.89 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        string str = "";
+        for(int i = 0; i < b; i++){
+            for(int j = 0; j < a; j++){
+                str += "*";
+            }
+            str += "\n";
+        }
+        
+        Console.WriteLine("{0}", str);
+    }
+}
