b849c6609ff85029a19aec538685a92a24b34386
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-02-20 10:36:56 +0900

    [level 1] Title: K번째수, Time: 2.97 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        List<int> answer = new List<int>();
+        for (int index = 0; index < commands.GetLength(0); index++)
+        {
+            int i = commands[index, 0];
+            int j = commands[index, 1];
+            int k = commands[index, 2];
+
+            List<int> temp = new List<int>();
+            for (int iter = i - 1; iter < j ; iter++)
+            {
+                temp.Add(array[iter]);
+            }
+            temp.Sort();
+            answer.Add(temp[k - 1]);
+        }
+        return answer.ToArray();
+
+    }
+}
