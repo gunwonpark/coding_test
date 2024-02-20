5d953e2b93490aa55e9c7c9db92bd40f213d1479
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-02-20 10:43:48 +0900

    [level 1] Title: K번째수, Time: 5.17 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cs
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -5,19 +5,17 @@ using System.Collections.Generic;
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
         List<int> answer = new List<int>();
+        List<int> temp = new List<int>(array);
+        
         for (int index = 0; index < commands.GetLength(0); index++)
         {
             int i = commands[index, 0];
             int j = commands[index, 1];
             int k = commands[index, 2];
 
-            List<int> temp = new List<int>();
-            for (int iter = i - 1; iter < j ; iter++)
-            {
-                temp.Add(array[iter]);
-            }
-            temp.Sort();
-            answer.Add(temp[k - 1]);
+            
+            List<int> newTemp = temp.Where((x, idx) => idx >= i - 1 && idx < j).OrderBy(x => x).ToList();
+            answer.Add(newTemp[k - 1]);
         }
         return answer.ToArray();
 
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748#) 
 
 ### 성능 요약
 
-메모리: 31.2 MB, 시간: 2.97 ms
+메모리: 31.4 MB, 시간: 5.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 10:36:53
+2024년 2월 2일 10:43:46
 
 ### 문제 설명
 
