d09103181fc54203c90425e5c24dd5d4b70a031b
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-04-16 22:32:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        a--;
+        b--;
+        while(a / 2 != b / 2){
+            a /= 2;
+            b /= 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
