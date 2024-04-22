3221b54a6b571c5213b59ae5d147f3c15a9f6f08
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-04-22 10:00:47 +0900

    [level 2] Title: N개의 최소공배수, Time: 29.68 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -2,49 +2,21 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] arr) {
-        int answer = 1;
-            int max = arr.Max();
-
-            for (int i = 2; i <= max; i++)
-            {
-                if (!IsPrime(i))
-                    continue;
-
-                if (arr.Max() == 1)
+        int answer = arr.Max();        
+        while(true){
+            bool IsSolved = true;
+            for(int i = 0; i < arr.Length; i++){
+                if(answer % arr[i] != 0){
+                    IsSolved = false;
                     break;
-
-                
-                while (true)
-                {
-                    bool tmp = false;
-                    int j = 0;
-                    foreach (var num in arr)
-                    {
-                        if (num % i == 0)
-                        {
-                            tmp = true;
-                            arr[j] /= i;
-                        }
-                        j++;
-                    }
-                    if (tmp)
-                        answer *= i;
-                    else
-                        break;
                 }
-               
-                
             }
-            return answer;
+            if(IsSolved)
+                break;
+            else
+                answer++;
         }
 
-        bool IsPrime(int num)
-        {
-            for (int i = 2; i < num; i++)
-            {
-                if (num % i == 0)
-                    return false;
-            }
-            return true;
-        }
+        return answer;
+    }
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953#) 
 
 ### 성능 요약
 
-메모리: 30.8 MB, 시간: 0.90 ms
+메모리: 31.3 MB, 시간: 29.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 09:55:01
+2024년 04월 22일 10:00:45
 
 ### 문제 설명
 
