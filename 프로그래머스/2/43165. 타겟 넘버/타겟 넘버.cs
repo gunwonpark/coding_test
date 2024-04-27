beabc57929b76b97d891f843d4a934da830e124d
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-04-27 20:26:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.40 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
@@ -2,23 +2,33 @@ using System;
 
 public class Solution {
      public int solution(int[] numbers, int target)
- {
-     int answer = 0;
-     DFS(numbers, target, 0, 0, ref answer);
-     return answer;
- }
- public void DFS(int[] numbers, int target, int index, int num, ref int answer)
- {
-     if (index == numbers.Length)
-     {
-         if (num == target)
-         {
-             answer += 1;
-             return;
-         }
-         return;
-     }
-     DFS(numbers, target, index + 1, num + numbers[index], ref answer);
-     DFS(numbers, target, index + 1, num - numbers[index], ref answer);
- }
+        {
+            int answer = 0;
+
+            for(int i = 0; i < Math.Pow(2,numbers.Length); i++)
+            {
+                int sum = 0;
+                int temp = i;
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if(temp % 2 == 1)
+                    {
+                        sum -= numbers[j];
+                    }
+                    else
+                    {
+                        sum += numbers[j];
+                    }
+                    temp /= 2;
+                }
+
+                if(sum == target)
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
 }
