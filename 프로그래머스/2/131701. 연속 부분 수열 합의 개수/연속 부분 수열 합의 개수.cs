52d4ccb19c955854f90cb5a9a8336d57ca603733
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-04-27 20:06:40 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 55.07 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cs
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cs
@@ -4,25 +4,24 @@ using System.Linq;
 
 public class Solution {
         public int solution(int[] elements)
-{
-    List<int> sums = new List<int>();
-    // elements를 원형 수열이라 가정하고 연속적인 수의 합들을 구해본다
-    for (int k = 1; k <= elements.Length; k++)
     {
-        for (int i = 0; i < elements.Length; i++)
+        int n = elements.Length;
+        HashSet<int> sum = new HashSet<int>();
+        int[,] dp = new int[n, n+ 1];
+        for(int i = 0; i < n; i++)
         {
-            int sum = 0;
-            for (int j = i; j < i + k; j++)
-            {
-                sum += elements[j % elements.Length];
-            }
-            sums.Add(sum);
+            dp[i, 1] = elements[i];
+            sum.Add(elements[i]);
         }
-    }
+        for(int i = 2; i <= n; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                dp[j, i] = dp[j, i - 1] + elements[(i + j - 1) % n];
+                sum.Add(dp[j, i]);
+            }
+        }
 
-    sums = sums.Distinct().ToList();
-    
-    return sums.Count;
-}
-    
+        return sum.Count;
+    }
 }
