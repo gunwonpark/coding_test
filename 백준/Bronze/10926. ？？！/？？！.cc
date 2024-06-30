19f1a0ce2cfac2222ce1fbc2ff5040f8bcb07c1c
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-06-30 23:38:27 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+    string str;
+    cin >> str;
+
+    cout << str + "??!";
+    
+    return 0;
+}
+
