8eafe6fe871ebd33fb33873c7cb1b3e40ea47779
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-07-03 23:51:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+#define ll long long
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);    
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c << endl;
+  
+
+
+    return 0;
+}
+
+
+
