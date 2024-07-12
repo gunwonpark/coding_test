55f62b1c47daf777fbd685e90afbeb8ae4091e6c
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-07-13 03:28:09 +0900

    [Platinum IV] Title: 대회 이름 정하기, Time: 536 ms, Memory: 45292 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/27933. 대회 이름 정하기/대회 이름 정하기.cc
+++ 백준/Platinum/27933. 대회 이름 정하기/대회 이름 정하기.cc
@@ -1,106 +1,171 @@
 #include <iostream>
-#include<algorithm>
-#include<typeinfo>
+#include <cstring>
+#include <algorithm>
+#include <utility>
 #include <vector>
-
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <cmath>
+#define inf 987654321
+#define ntimes(n, x) for(int x=0;x<n;x++)
+#define forall(v) v.begin(), v.end()
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple <int, int, int>;
 
-bool comp(pair<int, string> a, pair<int, string> b) {
-    return a.first < b.first;
-}
-int main() {
 
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    //연 = Y, 고 = K
-    //Y는 연고연고연고 = YKYKYK
-    //K는 고연고연고연 = KYKYKY
-    int n;
-    cin >> n;
+long long seg[2000100];
 
-    string card_set;
-    cin >> card_set;
+long long numlist[505000];
+long long blocklist[505000];
+long long lmax[505000];
+long long rmax[505000];
+pair<long long, char> border[505000];
+int n, t;
+string yklist;
 
-    vector<pair<char, int>> card(n);
-    for (int i = 0; i < card_set.size(); i++) {
-        card[i].first = card_set[i];
-        cin >> card[i].second;
+int segad;
+
+
+long long sum(int l, int r) {
+    long long res = 0;
+    l += segad; r += segad;
+    while (l <= r) {
+        if (l % 2)
+            res += seg[l++];
+        if (!(r % 2))
+            res += seg[r--];
+        l >>= 1; r >>= 1;
     }
-    
-    int t;
+    return res;
+}
+
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> n;
+    cin >> yklist;
+
+
+    cin >> numlist[0];
+    border[0].first = 0;
+    border[0].second = yklist[0];
+    int bpos = 0;
+    int mmax = numlist[0];
+    vector<int> maxlist;
+
+
+    for (int i = 1; i < n; i++) {
+        cin >> numlist[i];
+        if (border[bpos].second != yklist[i]) {
+            border[++bpos].first = i;
+            border[bpos].second = yklist[i];
+            maxlist.push_back(mmax);
+            mmax = numlist[i];
+        }
+        else {
+            if (mmax < numlist[i])
+                mmax = numlist[i];
+
+        }
+        lmax[i] = mmax;
+        blocklist[i] = bpos;
+    }
+    bpos++;
+    border[bpos].first = n;
+    maxlist.push_back(mmax);
+
+    int j = n - 1;
+    for (int i = bpos - 1; i >= 0; i--) {
+        int mmax = 0;
+        while (j >= border[i].first) {
+            if (mmax < numlist[j])  mmax = numlist[j];
+            rmax[j] = mmax;
+            j--;
+        }
+    }
+
+    segad = 1;
+    while (segad < bpos) segad *= 2;
+
+    for (int i = 0; i < maxlist.size(); i++) {
+        seg[segad + i] = maxlist[i];
+    }
+
+    for (int i = segad - 1; i > 0; i--) {
+        seg[i] = seg[2 * i] + seg[2 * i + 1];
+    }
+
     cin >> t;
-
-    int l, r;
-    long long y_player = 0, k_player = 0;
-
-    for (int i = 0; i < t; i++)
-    {
+    while (t--) {
+        int l, r;
         cin >> l >> r;
-        //doSolution
-        int y_tmp = 0;
-        int x_tmp = 0;
-        bool y_flag = true;
-        bool x_flag = true;
+        l--; r--;
+        int lb = -1; int rb = -1;
+        lb = blocklist[l];
+        rb = blocklist[r];
+        int yf = 1; int kf = 1;
 
-        //y_player에 대한 점수계산
-        for (int j = l-1; j <= r-1; j++) {
-            if (card[j].first == 'Y') {
-                if (x_flag == false) {
-                    y_player += y_tmp + x_tmp;
-                    y_tmp = 0;
-                    x_tmp = 0;
-                    x_flag = true;
-                }
-                y_tmp = max(card[j].second, y_tmp);
-                y_flag = false;
+        long long ysum = 0; int yl = lb; int yr = rb;
+        long long ksum = 0; int kl = lb; int kr = rb;
+
+        if (lb == rb) {
+            cout << "YK " << 0 << "\n";
+            continue;
+        }
+        if (rb - lb == 1) {
+            if (border[lb].second == 'Y') {
+                cout << "Y " << rmax[l] + lmax[r] << "\n";
             }
-            else if(card[j].first == 'K' && !y_flag){
-                x_tmp = max(card[j].second, x_tmp);
-                x_flag = false;
+            else {
+                cout << "K " << rmax[l] + lmax[r] << "\n";
             }
+            continue;
         }
 
-        //K로 끝날경우 마지막 경우를 더해준다
-        if(y_flag != true && x_flag == false)
-            y_player += y_tmp + x_tmp;
-
-        y_tmp = 0;
-        x_tmp = 0;
-        y_flag = true;
-        x_flag = true;
-
-        //x_player에 대한 점수계산
-        for (int j = l - 1; j <= r - 1; j++) {
-            if (card[j].first == 'K') {
-                if (x_flag == false) {
-                    k_player += y_tmp + x_tmp;
-                    y_tmp = 0;
-                    x_tmp = 0;
-                    x_flag = true;
-                }
-                y_tmp = max(card[j].second, y_tmp);
-                y_flag = false;
-            }
-            else if (card[j].first == 'Y' && !y_flag) {
-                x_tmp = max(card[j].second, x_tmp);
-                x_flag = false;
+        if (border[lb].second == 'Y') {
+            kl++;
+            if (l != border[lb].first) {
+                ysum += rmax[l];
+                yl++;
             }
         }
-
-        //Y로 끝날경우 마지막 경우를 더해준다
-        if (y_flag != true && x_flag == false)
-            k_player += y_tmp + x_tmp;
-        
-        if (y_player > k_player)
-            cout << "Y " << y_player << endl;
-        else if (y_player < k_player)
-            cout << "K " << k_player << endl;
-        else
-            cout << "YK " << y_player << endl;
-
-        y_player = 0;
-        k_player = 0;
+        else {
+            yl++;
+            if (l != border[lb].first) {
+                ksum += rmax[l];
+                kl++;
+            }
+        }
+        if (border[rb].second == 'Y') {
+            yr--;
+            if (r != border[rb + 1].first - 1) {
+                ksum += lmax[r];
+                kr--;
+            }
+        }
+        else {
+            kr--;
+            if (r != border[rb + 1].first - 1) {
+                ysum += lmax[r];
+                yr--;
+            }
+        }
+        if (yf) ysum += sum(yl, yr);
+        if (kf) ksum += sum(kl, kr);
+        if (ysum > ksum) {
+            cout << "Y " << ysum << "\n";
+        }
+        else if (ysum < ksum) {
+            cout << "K " << ksum << "\n";
+        }
+        else {
+            cout << "YK " << ysum << "\n";
+        }
     }
     return 0;
 }
