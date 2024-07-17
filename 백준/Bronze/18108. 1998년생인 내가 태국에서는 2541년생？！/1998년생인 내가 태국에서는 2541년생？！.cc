599bc5342494ddeb0cc3c18f62abb9ae125bd13e
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-07-17 23:59:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <cmath>
+
+#define FAST ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+    FAST;    
+    
+    int y;
+    cin >> y;
+
+    cout << y - 543;
+    
+    return 0;
+}
+
+
