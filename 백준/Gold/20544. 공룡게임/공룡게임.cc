61597ea6793de05b3709e8a35ec530950e39ca27
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-08-08 02:16:11 +0900

    [Gold IV] Title: 공룡게임, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/20544. 공룡게임/공룡게임.cc
+++ 백준/Gold/20544. 공룡게임/공룡게임.cc
@@ -28,7 +28,7 @@ int main() {
 	smallDp.resize(1001, 0);
 	bigDp.resize(1001, 0);
 
-	smallDp[0] = 0;
+	smallDp[0] = 1;
 	smallDp[1] = 2;
 	smallDp[2] = 4;
 	smallDp[3] = smallDp[2] + smallDp[1] + 1;
@@ -38,7 +38,7 @@ int main() {
 		smallDp[i] %= mod;
 	}
 
-	bigDp[1] = 1;
+	bigDp[1] = 0;
 	bigDp[2] = 1;
 	bigDp[3] = 4;
 	bigDp[4] = 10;
@@ -47,10 +47,7 @@ int main() {
 		bigDp[i] %= mod;
 	}
 
-	if(n == 1)
-		cout << 0 << "\n";
-	else
-		cout << bigDp[n] << "\n";
+	cout << bigDp[n] << "\n";
 
 	return 0;
 }
