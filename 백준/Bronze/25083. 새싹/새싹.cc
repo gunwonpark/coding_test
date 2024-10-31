75313ee5e0692916a1d6aedf7d322ad48d7e5640
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-11-01 03:08:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+#define Fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    Fast;
+ 
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+
+    
+    return 0;
+}
