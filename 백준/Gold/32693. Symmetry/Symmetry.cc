4a1953d077cb7f7c7453a5c3c517da28a1f46075
박건원 <54972800+gunwonpark@users.noreply.github.com>
2024-11-18 16:55:40 +0900

    [Gold IV] Title: Symmetry, Time: 72 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/32693. Symmetry/Symmetry.cc
+++ 백준/Gold/32693. Symmetry/Symmetry.cc
@@ -1,59 +1,40 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 #define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-
 using ll = long long;
-
 using namespace std;
-
 
 int main() {
     Fast;
-    
+
     int r, c;
     cin >> r >> c;
-    vector<string> a(r);    
+    vector<string> a(r);
     ll result = 0;
 
-    for (int i = 0; i < r; i++)
-    {
+    for (int i = 0; i < r; i++) {
         cin >> a[i];
     }
 
     for (int i = 0; i < c - 1; i++) {
-        // set 초기화
-        set<int> s;
-        for (int j = 0; j < r; j++) {
-            s.insert(j);
-        }
+        vector<int> active_rows(r, 1); // 유효한 행을 추적하는 배열
         int left = i, right = i + 1;
 
-        while (left >= 0 && right < c && !s.empty()) {
+        while (left >= 0 && right < c) {
             ll count = 0;
-            int prev = *s.begin() - 1;
-            set<int> t_se = s;
-            for (auto& se : t_se) {
-                int cur = se;
 
-                if (cur - prev != 1) {
-                    result += count * (count + 1) / 2;
-                    count = 0;
-                }
-
-                if (a[se][left] == a[se][right]) {
+            for (int j = 0; j < r; j++) {
+                if (active_rows[j] && a[j][left] == a[j][right]) {
                     count++;
-                }
-                else {
-                    s.erase(se);
+                } else {
                     result += count * (count + 1) / 2;
                     count = 0;
+                    active_rows[j] = 0; // 비활성화
                 }
-                prev = cur;
             }
 
-            result += count * (count + 1) / 2;
-            left -= 1;
-            right += 1;
+            result += count * (count + 1) / 2; // 남은 카운트 처리
+            left--;
+            right++;
         }
     }
 
