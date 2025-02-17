256630489351829bbc88f7002ef81be55a45cefd
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-02-18 01:16:45 +0900

    [Gold I] Title: 수열의 점수, Time: 0 ms, Memory: 2024 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/33489. 수열의 점수/수열의 점수.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#define FAST std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+typedef long long ll;
+
+using namespace std;
+
+ll fibo[501];
+
+int main() {
+	FAST;
+
+	int T; cin >> T;
+
+	fibo[0] = 0;
+	fibo[1] = 1;
+
+	for (int i = 2; i <= 500; i++) {
+		fibo[i] = (fibo[i - 1] + fibo[i - 2]);
+	}
+
+	while (T--) {
+		ll x, y;
+		cin >> x >> y;
+
+
+		int index = 0;
+
+		for (; index < 500; index++) {
+			if (fibo[index + 1] > y || fibo[index + 2] > x) {
+				break;
+			}
+		}
+		cout << fibo[index] << " " << fibo[index + 1] << '\n';
+	}
+
+	return 0;
+}
