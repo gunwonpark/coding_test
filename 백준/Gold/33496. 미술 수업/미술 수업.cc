809802c49f75a580b21cd275788c51cb2d7e242f
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-02-17 16:17:13 +0900

    [Gold V] Title: 미술 수업, Time: 0 ms, Memory: 2292 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Gold/33496. 미술 수업/미술 수업.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define FAST std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+	FAST;	
+
+	int n; cin >> n;
+	vector<int> left, right;
+	set<int> left_set, right_set;
+
+	int answer = 0;
+
+	for (int i = 0; i < n; i++) {
+		int x, y; cin >> x >> y;
+
+		int left_value = x - y;
+		int right_value = x + y;
+
+		auto left_it = left_set.find(left_value);
+		auto right_it = right_set.find(right_value);
+
+		if (left_it == left_set.end()) {
+			left_set.insert(left_value);
+			left.push_back(left_value);
+		}
+		if (right_it == right_set.end()) {
+			right_set.insert(right_value);
+			right.push_back(right_value);
+		}
+
+	}
+
+	sort(left.begin(), left.end());
+	sort(right.begin(), right.end());
+
+	set<int> total;
+
+	for (auto& l : left_set) {
+		total.insert(l);
+	}
+
+	for (auto& r : right_set) {
+		total.insert(r);
+	}
+
+	for (int i = 0; i < left.size(); i++) {
+		auto index = upper_bound(right.begin(), right.end(), left[i]) - right.begin();
+
+		answer += right.size() - index;
+	}
+
+	cout << answer + total.size();
+
+	
+
+	return 0;
+}
