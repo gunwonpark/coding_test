5e0eb5a2ddd2d82779bb25a81995873ebf4294d6
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-02-18 17:52:48 +0900

    [Gold V] Title: 피타고라스 정리의 증명, Time: 148 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#define FAST std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+
+typedef long long ll;
+
+using namespace std;
+
+
+
+int main() {
+	FAST;
+
+	int T; cin >> T;
+
+	while (T--) {
+		int n; cin >> n;
+		
+		if (n & 1) {
+			cout << n * 2 - 1 << '\n';
+		}
+		else {
+			cout << n * 2 << '\n';
+		}
+	}
+
+	return 0;
+}
