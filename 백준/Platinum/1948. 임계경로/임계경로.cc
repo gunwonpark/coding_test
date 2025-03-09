21fdf61ad0b8e1a005059769c021ffda4d9626d7
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-03-09 14:35:12 +0900

    [Platinum V] Title: 임계경로, Time: 356 ms, Memory: 4812 KB -BaekjoonHub

---
+++ 백준/Platinum/1948. 임계경로/임계경로.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define FAST std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+typedef long long ll;
+
+using namespace std;
+
+struct Compare {
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		return a.second > b.second;
+	}
+};;
+
+
+int main() {
+	FAST;
+
+	int n, m, s, e;
+	cin >> n >> m;
+	vector<vector<pair<int, int>>> adj(n + 1);
+
+	for (int i = 0; i < m; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back({ v, -w });
+	}
+
+	cin >> s >> e;
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+	vector<int> dist(n + 1, 1e9);
+	vector<vector<int>> path(n + 1);
+
+	pq.push({ s, 0 });
+
+	while (!pq.empty()) {
+		auto cur = pq.top();
+		int curNode = cur.first;
+		int curDist = cur.second;
+
+		pq.pop();
+
+		if (dist[curNode] < curDist) continue;
+
+		for (auto next : adj[curNode]) {
+			int nextNode = next.first;
+			int nextDist = curDist + next.second;
+			if (dist[nextNode] > nextDist) {
+				dist[nextNode] = nextDist;
+				pq.push({ nextNode, nextDist });
+				path[nextNode].clear();
+				path[nextNode].push_back(curNode);
+			}
+			else if (dist[nextNode] == nextDist) {
+				path[nextNode].push_back(curNode);
+			}
+		}
+	}
+
+	set<int> visited;
+
+	queue<int> q;
+	q.push(e);
+
+	int answer = 0;
+
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		if (visited.find(cur) != visited.end()) continue;
+		visited.insert(cur);
+		for (auto prev : path[cur]) {
+			q.push(prev);
+			answer++;
+		}
+	}
+
+	cout << -dist[e] << '\n';
+	cout << answer << '\n';
+
+    return 0;
+}
+
