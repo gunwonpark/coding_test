05abeded9fca06ecf2fc89219974f24328973d3b
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-03-27 19:25:07 +0900

    [Platinum V] Title: Parcel, Time: 44 ms, Memory: 2940 KB -BaekjoonHub

---
+++ 백준/Platinum/16287. Parcel/Parcel.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define FAST std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+typedef long long ll;
+
+using namespace std;
+
+int w, n;
+vector<int> arr;
+bool sums[800001]{};
+
+int main() {
+    FAST;
+
+    cin >> w >> n;
+    arr.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+    for (int i = 2; i <= n - 2; i++) {
+        for (int j = 0; j < i - 1; j++) {
+            sums[arr[i - 1] + arr[j]] = true;
+        }
+
+        for (int j = i + 1; j < n; j++) {
+            int value = w - arr[i] - arr[j];
+
+            if (value <= 0) continue;
+            if (sums[value]) {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+
+	cout << "NO";
+
+    return 0;
+}
