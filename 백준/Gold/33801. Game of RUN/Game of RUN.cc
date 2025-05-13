37dd9fd104e87d42f66d16a045e4df3cfafeb467
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-05-13 09:05:06 +0900

    [Gold IV] Title: Game of RUN, Time: 76 ms, Memory: 56744 KB, Score: 45 point -BaekjoonHub

---
+++ 백준/Gold/33801. Game of RUN/Game of RUN.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+
+int main()
+{
+	FAST;
+	int T; cin >> T;
+	vector<vector<ll>> dp(1000001, vector<ll>(3, 0));
+	dp[1][0] = 1;
+	dp[1][1] = 0;
+	dp[1][2] = 0;
+
+	dp[2][0] = 3;
+	dp[2][1] = 1;
+	dp[2][2] = 1;
+
+	ll tmpSum = 0;
+
+	for (int i = 3; i <= 1000000; i++) {
+		tmpSum += (dp[i - 2][1] + dp[i - 2][2]) % 1000000007;
+		dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + 2 + tmpSum) % 1000000007;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][1]) % 1000000007;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][2]) % 1000000007;
+	}
+
+	while (T--) {
+		int n; cin >> n;
+		cout << dp[n][0] + dp[n][1] + dp[n][2] << '\n';
+	}
+
+
+
+	return 0;
+}
