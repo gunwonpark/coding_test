7edf92b202f418b250a7d4bf19510b11a32d580d
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-05-20 09:55:56 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+	FAST;
+
+	ll n; cin >> n;
+
+	cout << n * n << " " << 2;
+	return 0;
+}
+
