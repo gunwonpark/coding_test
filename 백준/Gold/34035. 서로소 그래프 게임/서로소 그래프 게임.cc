b8cdecfb0ac5ce034a01cb80ac1c26187e8920b8
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-07-03 15:43:04 +0900

    [Gold III] Title: 서로소 그래프 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34035. 서로소 그래프 게임/서로소 그래프 게임.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+
+
+int main()
+{
+	FAST;
+
+	int T; cin >> T;
+	while (T--) {
+		int n; cin >> n;
+		if (n == 2)
+		{
+			cout << "kang";
+		}
+		else {
+			cout << "chan";
+		}
+
+		cout << '\n';
+	}
+	
+
+	return 0;
+}
+
