9fae1d4dfa912c2b9d104a3e88c846147a92c4e9
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-07-07 14:22:27 +0900

    [Gold II] Title: DagDag구리, Time: 824 ms, Memory: 24732 KB -BaekjoonHub

---
+++ 백준/Gold/34012. DagDag구리/DagDag구리.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+	FAST;
+
+	int n, m; cin >> n >> m;
+
+	map<int, vector<int>> adj;
+	map<int, int> outdegree;
+	set<int> nodes;
+	int E = 0;
+
+	for (int i = 0; i < m; i++) {
+		int u, v; cin >> u >> v;
+		adj[u].push_back(v);
+		outdegree[u]++;
+		nodes.insert(u);
+		nodes.insert(v);
+	}
+
+	for (auto& node : nodes) {
+		if (outdegree[node] == 0) {
+			E = node;
+			break;
+		}
+	}
+
+	int answer = 0;
+
+	for (auto& node : nodes)
+	{
+		if (node == E) continue;
+		if (outdegree[node] == 1)
+		{
+			answer++;
+		}
+	}
+
+	cout << answer;
+	
+	return 0;
+}
+
