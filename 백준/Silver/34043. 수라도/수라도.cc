b385e333befa52e96dd4e90f8d8a9cd367c7bbb9
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-08-08 15:25:17 +0900

    [Silver I] Title: 수라도, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/34043. 수라도/수라도.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+typedef long long ll;
+
+using namespace std;
+
+
+int main()
+{
+	FAST;
+	int n; cin >> n;
+	if (n != 1 && n != 3)
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+
+
+	
+
+	return 0;
+}
+
