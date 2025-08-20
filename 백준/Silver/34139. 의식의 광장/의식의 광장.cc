7c516e76b28bd377075aa1ffcd54ea8f72722f83
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-08-20 13:13:35 +0900

    [Silver II] Title: 의식의 광장, Time: 76 ms, Memory: 6760 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/34139. 의식의 광장/의식의 광장.cc
+++ 백준/Silver/34139. 의식의 광장/의식의 광장.cc
@@ -5,21 +5,44 @@
 
 using namespace std;
 
+struct Node
+{
+	int x, y, index;
+	Node(int x, int y, int index) : x(x), y(y), index(index) {};
+	Node() {};
+};
 
 int main()
 {
 	FAST;
 	int h, n; cin >> h >> n;
+	vector<Node> arr;
 
 	for (int i = 0; i < n; i++)
 	{
 		int a, b; cin >> a >> b;
+		arr.push_back({ a,b,i });
 	}
+
+	sort(arr.begin(), arr.end(), [](const Node& a, const Node& b) {
+		return a.y < b.y;
+		});
 
 	cout << "YES\n";
 
+	vector<int> answer;
+	answer.resize(n);
+
 	for (int i = 0; i < n; i++)
 	{
-		cout << i + 1 << " ";
+		int index = arr[i].index;
+		answer[index] = i + 1;
 	}
+
+	for (auto& ans : answer)
+	{
+		cout << ans << " ";
+	}
+
+	return 0;
 }
