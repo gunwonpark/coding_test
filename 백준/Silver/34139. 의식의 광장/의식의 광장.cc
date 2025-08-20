f4e93a455408f57e8d5470dcf940cc450deb4874
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-08-20 12:16:54 +0900

    [Silver II] Title: 의식의 광장, Time: 60 ms, Memory: 2020 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Silver/34139. 의식의 광장/의식의 광장.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+
+using namespace std;
+
+
+int main()
+{
+	FAST;
+	int h, n; cin >> h >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a, b; cin >> a >> b;
+	}
+
+	cout << "YES\n";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << " ";
+	}
+}
