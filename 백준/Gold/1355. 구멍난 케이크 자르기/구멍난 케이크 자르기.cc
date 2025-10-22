bd35b207cb84c91845426b81076fa1ea47bad886
박건원 <54972800+gunwonpark@users.noreply.github.com>
2025-10-23 05:02:23 +0900

    [Gold II] Title: 구멍난 케이크 자르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1355. 구멍난 케이크 자르기/구멍난 케이크 자르기.cc
+++ 백준/Gold/1355. 구멍난 케이크 자르기/구멍난 케이크 자르기.cc
@@ -5,91 +5,103 @@
 
 using namespace std;
 
-int dir[4][2] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
-
-void dfs(vector<vector<int>>& board, int x, int y)
-{
-    board[x][y] = 0;
-    for(int i = 0; i < 4; i++)
-    {
-        int nx = x + dir[i][0];
-        int ny = y + dir[i][1];
-        if(nx < 0 || ny < 0 || nx >= board.size() || ny >= board.size()) continue;
-        if(board[nx][ny] == 1)
-        {
-            dfs(board, nx, ny);
-        }
-	}
-}
-
 int main() {
     FAST;
     int n, m; cin >> n >> m;
-
-    vector<vector<int>> board(4 * n + 1, vector<int>(4 * n + 1, 1));
-
-    int mid = board.size() / 2;
-
-    for (int i = mid - 2 * m; i <= mid + 2 * m; i++)
-    {
-        for (int j = mid - 2 * m; j <= mid + 2 * m; j++)
-        {
-            board[i][j] = 0;
-        }
-    }
-
+    set<int> horizontalCuts;
+    set<int> verticalCuts;
     int h; cin >> h;
+
     for (int i = 0; i < h; i++) {
         int cut; cin >> cut;
-
-        int keyPoint = mid + 2 * cut;
-
-        for (int j = 0; j < board.size(); j++)
-        {
-            board[keyPoint][j] = 0;
-        }
+        horizontalCuts.insert(cut);
     }
-
     int v; cin >> v;
     for (int i = 0; i < v; i++) {
         int cut; cin >> cut;
-
-        int keyPoint = mid + 2 * cut;
-
-        for (int j = 0; j < board.size(); j++)
-        {
-            board[j][keyPoint] = 0;
-        }
+        verticalCuts.insert(cut);
     }
 
-    int answer = 0;
+    ll answer = 1;
+    int intervalCount = 0;
+    int insideCount = 0;
+    bool isInterval = false;
+    bool isFirst = true;
 
-    for (int i = 0; i < board.size(); i++)
+    for (auto& cut : horizontalCuts)
     {
-        for (int j = 0; j < board.size(); j++)
+        int horizontalCut = cut;
+
+        if (horizontalCut >= -m && horizontalCut <= m)
         {
-            if (board[i][j] == 1)
+            insideCount++;
+            isInterval = true;
+        }
+        else
+        {
+            intervalCount++;
+            isInterval = false;
+        }
+
+        if (isInterval)
+        {
+            if (isFirst)
             {
-                answer++;
-                dfs(board, i, j);
-
-                /*for (int x = 0; x < board.size(); x++)
-                {
-                    for (int y = 0; y < board.size(); y++)
-                    {
-                        cout << board[x][y];
-                    }
-                    cout << '\n';
-                }
-
-                cout << '\n';*/
+                answer += 1;
+                isFirst = false;
+            }
+            else
+            {
+                answer += 2;
             }
         }
+        else
+        {
+            answer += 1;
+        }
     }
 
-    
-	cout << answer << '\n';
+    isInterval = false;
+    isFirst = true;
 
+    for (auto& cut : verticalCuts)
+    {
+        int verticalCut = cut;
+        if (verticalCut >= -m && verticalCut <= m)
+        {
+            isInterval = true;
+        }
+        else
+        {
+            isInterval = false;
+        }
+
+        if (isInterval)
+        {
+            if (isFirst)
+            {
+                if (insideCount > 0)
+                {
+					answer += 2 + intervalCount;
+                }
+                else
+                {
+					answer += 1 + intervalCount;
+                }
+                isFirst = false;
+            }
+            else
+            {
+                answer += 2 + intervalCount;
+            }
+        }
+        else
+        {
+            answer += horizontalCuts.size() + 1;
+        }
+    }
+
+    cout << answer << "\n";
 
     return 0;
 }
